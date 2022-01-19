79a1b10fa7af7ecb83e6bcc890ddf7f9b528ee96
decagon <godsonjonathan39@gmail.com>
2022-01-19 18:30:22 +0100

    Updated Spring Security

---
+++ src/main/java/com/decagon/fitnessoapp/utils/Api_Uri.java
@@ -0,0 +1,10 @@
+package com.decagon.fitnessoapp.utils;
+
+
+public class Api_Uri {
+    public static String [] PUBLIC_URIs = {
+            "/v2/api-docs", "/configuration/**", "/swagger*/**","/swagger-ui/**", "/webjars/**",
+            "/login",
+            "/sign"
+    };
+}
