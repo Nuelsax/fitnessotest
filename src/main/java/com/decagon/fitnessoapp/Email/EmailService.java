9779509aa81142da07653914927e5dca6da70c7f
TsalachSnow <prosper.amalaha@decagon.dev>
2022-01-24 21:03:18 +0100

    Email service Integration with Mailjet completed

--- src/main/java/com/decagon/fitnessoapp/Email/EmailService.java
+++ src/main/java/com/decagon/fitnessoapp/Email/EmailService.java
@@ -1,2 +1,14 @@
-package com.decagon.fitnessoapp.Email;public interface EmailService {
+package com.decagon.fitnessoapp.Email;
+
+import com.mailjet.client.errors.MailjetException;
+import com.mailjet.client.errors.MailjetSocketTimeoutException;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface EmailService {
+//    ResponseEntity<String> sendMessage(String email, String text) throws UnirestException;
+void sendMessage(String email, String text, String Cname) throws MailjetException, MailjetSocketTimeoutException;
 }
