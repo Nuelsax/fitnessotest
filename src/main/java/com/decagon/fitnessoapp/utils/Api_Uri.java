8e7eeea5b0dc966ee1a07a80806f082fabb7a015
Ceejay <anumadu.chijindu@decagon.dev>
2022-01-27 16:02:08 +0100

    update

--- src/main/java/com/decagon/fitnessoapp/utils/Api_Uri.java
+++ src/main/java/com/decagon/fitnessoapp/utils/Api_Uri.java
@@ -5,7 +5,7 @@ public class Api_Uri {
     public static String [] PUBLIC_URIs = {
             "/v2/api-docs", "/configuration/**", "/swagger*/**","/swagger-ui/**", "/webjars/**",
             "/login",
-            "/sign"
+            "/sign",
     };
 
 //    public static String API_KEY = System.getenv().get("");
