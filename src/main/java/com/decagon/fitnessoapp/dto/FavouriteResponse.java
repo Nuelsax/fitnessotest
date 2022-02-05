a7b05f8f8f203b7314dfa8c3b11ab5e74849a0f7
decagon <godsonjonathan39@gmail.com>
2022-02-05 01:16:56 +0100

    Add, Delete and view favourites

--- src/main/java/com/decagon/fitnessoapp/dto/FavouriteResponse.java
+++ src/main/java/com/decagon/fitnessoapp/dto/FavouriteResponse.java
@@ -11,31 +11,16 @@ public class FavouriteResponse {
     private Long id;
     private Long productId;
     private Long personId;
-//    private String productName;
-//    private String prouctName;
-//    private BigDecimal price;
-//    private String description;
-//    private Long stock;
-//    private String productType;
-//    private String image;
-//    private Integer durationInHourPerDay;
-//    private Integer durationInDays;
-//    private Integer quantity;
+    private String productName;
+    private String prouctName;
+    private BigDecimal price;
+    private String description;
+    private Long stock;
+    private String productType;
+    private String image;
+    private Integer durationInHourPerDay;
+    private Integer durationInDays;
+    private Integer quantity;
 
-//    @Override
-//    public String toString() {
-//        return "FavouriteResponse{" +
-//                "id=" + id +
-//                ", productName='" + productName + '\'' +
-//                ", prouctName='" + prouctName + '\'' +
-//                ", price=" + price +
-//                ", description='" + description + '\'' +
-//                ", stock=" + stock +
-//                ", productType='" + productType + '\'' +
-//                ", image='" + image + '\'' +
-//                ", durationInHourPerDay=" + durationInHourPerDay +
-//                ", durationInDays=" + durationInDays +
-//                ", quantity=" + quantity +
-//                '}';
-//    }
+
 }
