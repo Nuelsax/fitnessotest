e620901ce37158a1d49e7f092090a1d87f458341
NJBodam <bodambest@yahoo.com>
2022-02-11 01:53:01 +0100

    finishing touches

--- src/main/java/com/decagon/fitnessoapp/dto/OrderResponse.java
+++ src/main/java/com/decagon/fitnessoapp/dto/OrderResponse.java
@@ -7,6 +7,7 @@ import com.decagon.fitnessoapp.model.product.TRANSACTION_STATUS;
 import com.decagon.fitnessoapp.model.user.Address;
 import com.decagon.fitnessoapp.model.user.PaymentCard;
 import com.decagon.fitnessoapp.model.user.Person;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,6 +22,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class OrderResponse {
 
     private String firstName;
