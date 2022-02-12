a923ed56e6c1ea6a454ef8f66deb65720168d255
Francis Oyiogu <oyioguf@decagonhq.com>
2022-02-12 23:07:03 +0100

    Update Api_Uri.java

--- src/main/java/com/decagon/fitnessoapp/utils/Api_Uri.java
+++ src/main/java/com/decagon/fitnessoapp/utils/Api_Uri.java
@@ -4,7 +4,7 @@ package com.decagon.fitnessoapp.utils;
 public class Api_Uri {
 
     public static String [] PUBLIC_URIs = {
-            "/v2/api-docs", "/configuration/**", "/swagger*/**","/swagger-ui/**", "/webjars/**", "/person/**", "/address/**",
+            "/v2/api-docs", "/configuration/**", "/swagger*/**","/swagger-ui/**", "/webjars/**", "/person*/**", "/address/**",
             "/product/**", "/carts/add/**", "/images/**"
     };
 }
